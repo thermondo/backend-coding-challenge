c60f040e578ffd77ffd514c7816f91d0796dda56
Dimuthu <dimuthuroshan@Dimuthus-MacBook-Pro.local>
2025-05-25 14:27:28 +0200

    initialize project strcture

---
+++ movie/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.openapi.generator") version "7.12.0"
+}
+
+group = "com.entertainment"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+openApiGenerate {
+	generatorName = "spring"
+	inputSpec = "$rootDir/specs/movie-api-spec.yaml"
+	outputDir = "${layout.buildDirectory}/generated".toString()
+	apiPackage = "com.entertainment.movie.api"
+	invokerPackage = "org.openapi.example.invoker"
+	modelPackage = "com.entertainment.movie.dto"
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.named("compileJava") {
+	dependsOn(tasks.openApiGenerate)
+}
