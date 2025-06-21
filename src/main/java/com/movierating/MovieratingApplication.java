d2814f1f50162522414dd390db1105a62375d619
Pranav S Kurup <pranavs.nkm@gmail.com>
2025-06-21 13:56:15 +0200

    [Initial Setup] Add Maven Wrapper and Spring Boot Application Structure

---
+++ src/main/java/com/movierating/MovieratingApplication.java
@@ -0,0 +1,13 @@
+package com.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieratingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movierating/MovieratingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieratingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
